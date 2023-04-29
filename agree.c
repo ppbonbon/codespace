9c64b10a4579aa88baab4c2d1e25f974c927436e
ppbonbon <132128324+ppbonbon@users.noreply.github.com>
2023-04-29 09:38:33 +0000

    Sat, Apr 29, 2023, 9:38 AM +00:00

--- agree.c
+++ agree.c
@@ -4,4 +4,13 @@
 int main(void)
 {
     char c = ger_cgar("DO you agreee? ");
+
+    if (c == 'y')
+    {
+        printf("Agreed.\n");
+    }
+    else if (c == 'n');
+    {
+        printf("Not agreed.");
+    }
 }
